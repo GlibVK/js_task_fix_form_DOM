3868ddd2f827a87fdd6808f31b025db85e5f9cda
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-13 13:12:52 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const inputs = document.querySelectorAll("input");
+for (const input of inputs){
+    const label = document.createElement("label");
+    const attributeName = separateWords(input.name);
+    label.htmlFor = input.id;
+    label.innerText = attributeName;
+    label.className = "field-label";
+    input.placeholder = attributeName;
+    input.before(label);
+}
+function separateWords(word) {
+    let separatedStr = "";
+    [
+        ...word
+    ].forEach((ch, i)=>{
+        if (i === 0) separatedStr += ch.toUpperCase();
+        else if (ch === ch.toUpperCase()) separatedStr += " " + ch;
+        else separatedStr += ch;
+    });
+    return separatedStr;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
